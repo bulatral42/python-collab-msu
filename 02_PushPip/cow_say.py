b6217b399d6c98b5caf13e76fd8c995e497a300d
bulat_v <valiahmetovbulat@mail.ru>
2023-02-19 10:54:00 +0300

    cowsay -l option & reading feom stdin

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -1,5 +1,5 @@
 from argparse import ArgumentParser
-from cowsay import cowsay
+from cowsay import cowsay, list_cows
 import sys
 
 
@@ -17,9 +17,26 @@ def cow_modes():
 
 
 def emulate_cow(args):
+    if args.l:
+        print('List of available cow files:', ' '.join(list_cows()), sep='\n')
+        return
+
+    # generate mode
     if args.mode is not None:
         args.mode = ''.join(args.mode)
+    # generate message or wait for input
+    if len(args.message) == 0:
+        message = []
+        for line in sys.stdin:
+            message.append(line.strip())
+        args.message = message
     args.message = ' '.join(args.message)
+    use_cowfile = '/' in args.f
+
+    if not use_cowfile and not args.f in list_cows():
+        print(f'cowsay: Could not find {args.f} cowfile!')
+        return
+
     print(
         cowsay(
             message=args.message,
@@ -28,7 +45,8 @@ def emulate_cow(args):
             eyes=args.e[:2],
             tongue=args.T[:2],
             width=args.W,
-            wrap_text=args.n
+            wrap_text=args.n,
+            cowfile=args.f if use_cowfile else None
         )
     )
 
@@ -37,13 +55,15 @@ def emulate_cow(args):
 if __name__ == '__main__':
     parser = ArgumentParser(prog='Cowsay', description='Emulation of cowsay command')
 
-    parser.add_argument('message', type=str, default=' ', action='store', nargs='*', help="message for cow to say")
+    parser.add_argument('message', type=str, default='', action='store', nargs='*', help="message for cow to say")
 
     parser.add_argument('-e', type=str, default='oo', action='store', metavar='eye_string', help="select the appearance of the cow's eyes")
     parser.add_argument('-f', type=str, default='default', metavar='cowfile', help='cow picture file')
     parser.add_argument('-n', action='store_false', help='do not word-wrap message')
     parser.add_argument('-T', type=str, action='store', default='  ', metavar='tongue_string', help="select the appearance of the cow's tongue")
     parser.add_argument('-W', type=int, action='store', default=40, metavar='column', help='column where the message should be wrapped')
+
+    parser.add_argument('-l', action='store_true', help='list all cowfiles')
 
     for mode, descr in cow_modes().items():
         parser.add_argument(f'-{mode}', action='append_const', dest='mode', const=mode, help='mode: ' + descr)

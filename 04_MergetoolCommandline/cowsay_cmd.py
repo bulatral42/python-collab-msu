f9e55cf0ea65330751509b68bab61e0b5815fb1c
bulat_v <valiahmetovbulat@mail.ru>
2023-03-05 16:10:48 +0300

    make_bubble command

--- 04_MergetoolCommandline/cowsay_cmd.py
+++ 04_MergetoolCommandline/cowsay_cmd.py
@@ -1,6 +1,7 @@
 import cowsay as cs
 import cmd
 import shlex
+import sys
 
 
 class CmdCowsay(cmd.Cmd):
@@ -10,9 +11,27 @@ class CmdCowsay(cmd.Cmd):
     def do_make_bubble(self, args):
         '''
         Wraps text into a bubble
-        make_bubble [cowsay | cowthink] Wrap text inside a bubble
+        make_bubble text [cowsay | cowthink] Wrap text inside a bubble
         '''
-        pass
+        parsed = shlex.split(args)
+        if len(parsed) == 0:
+            print("No text to bubble")
+            return
+
+        if len(parsed) > 0:
+            text = parsed[0]
+
+        if len(parsed) > 1:
+            if parsed[1] in ['cowsay', 'cowthink']:
+                brackets = cs.THOUGHT_OPTIONS[parsed[1]]
+            else:
+                print("Wrong brackets parameter")
+                return
+        else:
+            brackets = cs.THOUGHT_OPTIONS['cowsay']
+
+        print(cs.make_bubble(text, brackets))
+
 
     def do_cowsay(self, args):
         '''

3414ac6d325c0eefc80ad057b4683dcf34d8484a
bulat_v <valiahmetovbulat@mail.ru>
2023-03-05 15:07:44 +0300

    list_cows command and emptyline change

--- 04_MergetoolCommandline/cowsay_cmd.py
+++ 04_MergetoolCommandline/cowsay_cmd.py
@@ -7,33 +7,39 @@ class CmdCowsay(cmd.Cmd):
     intro = "Welcome to cow farm!"
     prompt = "moo? "
 
-    def do_make_bubble(self, arg):
+    def do_make_bubble(self, args):
         '''
         Wraps text into a bubble
         make_bubble [cowsay | cowthink] Wrap text inside a bubble
         '''
         pass
 
-    def do_cowsay(self, arg):
+    def do_cowsay(self, args):
         '''
         Emulate cowsay command: express cow's words
         cowsay message [cow [eyes [tongue]]]
         '''
         pass
 
-    def do_cowthink(self, arg):
+    def do_cowthink(self, args):
         '''
         Emulate cowthink command: express cow's thoughts (similar to cowsay)
         cowthink message [cow [eyes [tongue]]]
         '''
         pass
 
-    def do_list_cows(self, arg):
+    def do_list_cows(self, args):
         '''
         Lists all cow file names in the given or default directory
         list_cows [cowpath]
         '''
-        pass
+        parsed = shlex.split(args)
+        if len(args) > 0:
+            print(*cs.list_cows(parsed[0]))
+        else:
+            print(*cs.list_cows())
+
+
 
     def do_exit(self, args):
         '''
@@ -48,10 +54,7 @@ class CmdCowsay(cmd.Cmd):
         return line
 
     def emptyline(self):
-        '''
-        '''
-        self.do_exit(None)
-        return 1
+        print("moo!")
 
 
 if __name__ == "__main__":

9120eb24ea84c36082a704e089bedb0610b86636
bulat_v <valiahmetovbulat@mail.ru>
2023-03-09 12:34:18 +0300

    messages are now commands
    Pparsing with shlex. Added who, cows, quit

--- 05_DiffPatchNet/cow_chat.py
+++ 05_DiffPatchNet/cow_chat.py
@@ -1,8 +1,12 @@
 import asyncio
 import cowsay as cs
+import shlex
 
 
 clients = {}
+free_names = set(cs.list_cows())
+used_names = set()
+
 
 class CowUser:
     self_id = None
@@ -17,6 +21,7 @@ class CowUser:
         self._port = port
         self._name = name
         self._q = asyncio.Queue()
+        print(f"User#{self._id} (connected)")
 
     def id(self):
         return self._id
@@ -31,22 +36,34 @@ class CowUser:
         return self._name
 
     def login(self, name):
-        # if name in cs.list_cows():
-        #     self._name = name
-        self._name = name
+        if name in free_names:
+            free_names.remove(name)
+            used_names.add(name)
+            self._name = name
+            print(f"User#{self._id} (logged in as) {self._name}")
         return not self._name is None
 
     def is_logged_in(self):
         return not self._name is None
 
+    async def who(self):
+        await self._q.put(f"{repr(used_names)}")
+
+    async def cows(self):
+        await self._q.put(f"{repr(list(free_names))}")
+
+    async def quit(self):
+        self._name = None
+        print(f"User#{self._id} (quit)")
+
+
+
 
 async def chat(reader, writer):
     ip, port = writer.get_extra_info('peername')
     me = CowUser(ip, port)
-    print(f"User#{me.id()} (connected)")
     clients[me.id()] = me
-    me.login(f"u%{me.id()}")
-    print(f"User#{me.id()} (logged in as) {me.name()}")
+    me.login(next(iter(free_names)))
 
     send = asyncio.create_task(reader.readline())
     receive = asyncio.create_task(me._q.get())
@@ -55,19 +72,26 @@ async def chat(reader, writer):
         for task in done:
             if task is send:
                 send = asyncio.create_task(reader.readline())
-                for user in clients.values():
-                    if user is not me:
-                        await user._q.put(f"{me.name()} {task.result().decode().strip()}")
+
+                parsed = shlex.split(task.result().decode())
+                match parsed:
+                    case ['who']:
+                        await me.who()
+                    case ['cows']:
+                        await me.cows()
+                    case ['quit']:
+                        await me.quit()
+                        send.cancel()
+                        del clients[me.id()]
+                        writer.close()
+                        await writer.wait_closed()
+                        break
             elif task is receive:
                 receive = asyncio.create_task(me._q.get())
+
                 writer.write(f"{task.result()}\n".encode())
                 await writer.drain()
-    send.cancel()
-    receive.cancel()
-    print(f"User#{me.id()} (quit)")
-    del clients[me.id()]
-    writer.close()
-    await writer.wait_closed()
+
 
 async def main():
     server = await asyncio.start_server(chat, '0.0.0.0', 1337)

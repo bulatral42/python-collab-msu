ad7dd57a652a2d7b961b22d3c5cb38ed7e4d2634
bulat_v <valiahmetovbulat@mail.ru>
2023-03-09 11:00:03 +0300

    CowUser class, names

--- 05_DiffPatchNet/cow_chat.py
+++ 05_DiffPatchNet/cow_chat.py
@@ -1,30 +1,71 @@
 import asyncio
+import cowsay as cs
+
 
 clients = {}
 
+class CowUser:
+    self_id = None
+    _ip = None
+    _port = None
+    _name = None
+    _q = None
+
+    def __init__(self, ip, port, name = None):
+        self._id = f"{ip}:{port}"
+        self._ip = ip
+        self._port = port
+        self._name = name
+        self._q = asyncio.Queue()
+
+    def id(self):
+        return self._id
+
+    def ip(self):
+        return self._ip
+
+    def port(self):
+        return self._port
+
+    def name(self):
+        return self._name
+
+    def login(self, name):
+        # if name in cs.list_cows():
+        #     self._name = name
+        self._name = name
+        return not self._name is None
+
+    def is_logged_in(self):
+        return not self._name is None
+
+
 async def chat(reader, writer):
-    me = "{}:{}".format(*writer.get_extra_info('peername'))
-    print(me)
-    clients[me] = asyncio.Queue()
+    ip, port = writer.get_extra_info('peername')
+    me = CowUser(ip, port)
+    print(f"User#{me.id()} (connected)")
+    clients[me.id()] = me
+    me.login(f"u%{me.id()}")
+    print(f"User#{me.id()} (logged in as) {me.name()}")
+
     send = asyncio.create_task(reader.readline())
-    receive = asyncio.create_task(clients[me].get())
+    receive = asyncio.create_task(me._q.get())
     while not reader.at_eof():
         done, pending = await asyncio.wait([send, receive], return_when=asyncio.FIRST_COMPLETED)
-        print(f"COMPLETE: {len(done)}")
-        for q in done:
-            if q is send:
+        for task in done:
+            if task is send:
                 send = asyncio.create_task(reader.readline())
-                for out in clients.values():
-                    if out is not clients[me]:
-                        await out.put(f"{me} {q.result().decode().strip()}")
-            elif q is receive:
-                receive = asyncio.create_task(clients[me].get())
-                writer.write(f"{q.result()}\n".encode())
+                for user in clients.values():
+                    if user is not me:
+                        await user._q.put(f"{me.name()} {task.result().decode().strip()}")
+            elif task is receive:
+                receive = asyncio.create_task(me._q.get())
+                writer.write(f"{task.result()}\n".encode())
                 await writer.drain()
     send.cancel()
     receive.cancel()
-    print(me, "DONE")
-    del clients[me]
+    print(f"User#{me.id()} (quit)")
+    del clients[me.id()]
     writer.close()
     await writer.wait_closed()
 
